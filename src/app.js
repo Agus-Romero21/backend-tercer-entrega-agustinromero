e2a5b46efbd67167cc1355c6619b407df053b9c7
Agustin Romero <agustinromero782@gmail.com>
2024-04-06 23:32:00 -0300

    problemas con el array solucionados, pequeño error al ejecutar el metodo getProductById

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
 const express = require('express')
 const app = express()
 
-const { ProductsManager } = require('./productManager')
-const prods = new ProductsManager()
 
+
+const { ProductsManager } = require("./productManager");
+
+const path = 'file/Products.json' 
+const products = new ProductsManager(path);
 
 
 app.get('/productos', async (req, res) => {
     const {limit} = req.query
+    const response = await products.getProducts()
 
-	const product = await prods.getProducts()
-	console.log(product)
+    if(!limit) return res.send(response)
+    const prodFound = response.filter (p => p.id === parseInt(limit))
+    res.send(prodFound)
+})
 
-    if(!limit) return res.send(product)
+app.get ('/productos/:pid', async (req,res) => {
+    const {pid} = req.params
 
-    const prodFound = product.filter (p => p.id === parseInt(limit))
-     res.send(prodFound)
+    const productFound = await products.getProductById(parseInt(pid))
+    if(productFound)res.send(productFound) 
+
+    res.send('el producto no fue encontrado')
 })
 
 app.listen(8080, error => {
